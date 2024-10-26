3e2e592d10c34e559dd1d4f8bc563a25ee70cbf1
Flotiarenor <flotiarenor@outlook.com>
2024-10-26 13:55:27 +0800

    1.23

--- env.py
+++ env.py
@@ -4,7 +4,6 @@ import torch
 class BinancePerpetualContract():
     def __init__(self, data: ndarray) -> None:
 
-
         #手续费
         self.Maker_conmission = 0.02/100
         self.Taker_conmission = 0.05/100
